15e54e5693b3dfe975ff9ab8af0e3238fc08a0f5
arashparnia <arashparnia@gmail.com>
2015-09-29 20:08:31 -0700

    sphere system added

--- src/a2/Ass2.java
+++ src/a2/Ass2.java
@@ -8,6 +8,8 @@ import com.jogamp.opengl.awt.GLCanvas;
 import graphicslib3D.GLSLUtils;
 import graphicslib3D.Matrix3D;
 import graphicslib3D.MatrixStack;
+import graphicslib3D.Vertex3D;
+import graphicslib3D.shape.Sphere;
 
 import javax.swing.*;
 import java.awt.*;
@@ -27,7 +29,7 @@ public class Ass2 extends JFrame implements GLEventListener, ActionListener, Mou
     private GLCanvas myCanvas;
     private MatrixStack mvStack = new MatrixStack(5);
     private int vao[] = new int[1];
-    private int vbo[] = new int[2];
+    private int vbo[] = new int[3];
     private float cameraX, cameraY, cameraZ;
     private float cubeLocX, cubeLocY, cubeLocZ;
     private float skyBoxLocX, skyBoxLocY, skyBoxLocZ;
@@ -35,43 +37,26 @@ public class Ass2 extends JFrame implements GLEventListener, ActionListener, Mou
     private int VAO[] = new int[1];
     private GLSLUtils util = new GLSLUtils();
     private float upDown = 0.0f;
-    private float g = 1f;
     private boolean animated = true;
     private Animator animator;
     private boolean autozoom = true;
-    private float n = 0.0f;
-    private float sp = 1.0f;
-    private float rotationi = 0.0f;
+
     private Random rand;
 
+    private Sphere mySphere = new Sphere(100);
+
     public Ass2() {
-        //JPanel panel = new JPanel();
-        //panel.setLayout(null);
-        //panel.setBounds(0, 0, this.getWidth(), 50);
-        //get the "focus is in the window" input map for the center panel
         int mapName = JComponent.WHEN_IN_FOCUSED_WINDOW;
         InputMap imap = this.getRootPane().getInputMap(mapName);
-        //create a keystroke object to represent the "f" key
-        KeyStroke upKey = KeyStroke.getKeyStroke("UP");
-        KeyStroke downKey = KeyStroke.getKeyStroke("DOWN");
-
-        //put the "f-Key" keystroke object into the panel’s "when focus is
-        // in the window" input map under the identifier "fire"
+        KeyStroke upKey = KeyStroke.getKeyStroke("w");
+        KeyStroke downKey = KeyStroke.getKeyStroke("s");
         ActionMap amap = this.getRootPane().getActionMap();
         imap.put(upKey, "zoomin");
         imap.put(downKey, "zoomout");
-
-        //get the action map for the panel
-
-        //put the "fireCommand" object (an instance of class "fireCommand";
-        // an Action object created elsewhere that performs the “fire” operation)
-        // into the panel's actionMap
         zoomIn zoomin = new zoomIn();
         amap.put("zoomin", zoomin);
         zoomOut zoomout = new zoomOut();
         amap.put("zoomout", zoomout);
-
-        //have the frame request keyboard focus
 
         setTitle("Assignment 2 CSC155");
         setSize(dimention);
@@ -79,54 +64,8 @@ public class Ass2 extends JFrame implements GLEventListener, ActionListener, Mou
         myCanvas = new GLCanvas();
         myCanvas.addGLEventListener(this);
         rand = new Random();
-//
-//        JButton up = new JButton("UP");
-//        up.setVerticalTextPosition(AbstractButton.CENTER);
-//        up.setHorizontalTextPosition(AbstractButton.CENTER);
-//        up.setMnemonic(KeyEvent.VK_W);
-//        up.setActionCommand("up");
-//        up.setBounds(5, 0, 100, 50);
-//        up.addActionListener(this);
-//        add(up);
-//        JButton down = new JButton("DOWN");
-//        down.setVerticalTextPosition(AbstractButton.CENTER);
-//        down.setHorizontalTextPosition(AbstractButton.CENTER);
-//        down.setMnemonic(KeyEvent.VK_KP_DOWN);
-//        down.setActionCommand("down");
-//        down.setBounds(110, 0, 100, 50);
-//        down.addActionListener(this);
-//        add(down);
-//        JButton color = new JButton("COLOR");
-//        color.setVerticalTextPosition(AbstractButton.CENTER);
-//        color.setHorizontalTextPosition(AbstractButton.CENTER);
-//        color.setMnemonic(KeyEvent.VK_KP_DOWN);
-//        color.setActionCommand("color");
-//        color.setBounds(215, 0, 100, 50);
-//        color.addActionListener(this);
-//        add(color);
-//        JButton nse = new JButton("NOISE");
-//        nse.setVerticalTextPosition(AbstractButton.CENTER);
-//        nse.setHorizontalTextPosition(AbstractButton.CENTER);
-//        nse.setMnemonic(KeyEvent.VK_KP_DOWN);
-//        nse.setActionCommand("noise");
-//        nse.setBounds(320, 0, 100, 50);
-//        nse.addActionListener(this);
-//        add(nse);
-//        JButton pause = new JButton("AUTOZOOM");
-//        pause.setVerticalTextPosition(AbstractButton.CENTER);
-//        pause.setHorizontalTextPosition(AbstractButton.CENTER);
-//        pause.setMnemonic(KeyEvent.VK_KP_DOWN);
-//        pause.setActionCommand("autozoom");
-//        pause.setBounds(425, 0, 100, 50);
-//        pause.addActionListener(this);
-//        add(pause);
-
-
-        //this.setLayout(null);
-        //this.add(panel);
         add(myCanvas);
         setVisible(true);
-
         this.addWindowListener(new WindowAdapter() {
             @Override
             public void windowClosing(WindowEvent windowEvent) {
@@ -163,8 +102,6 @@ public class Ass2 extends JFrame implements GLEventListener, ActionListener, Mou
     }
 
     public void display(GLAutoDrawable drawable) {
-        FloatBuffer noise = FloatBuffer.allocate(1);
-        noise.put(0, n);
         GL4 gl = (GL4) drawable.getGL();
 
         gl.glClear(GL_DEPTH_BUFFER_BIT);
@@ -179,25 +116,20 @@ public class Ass2 extends JFrame implements GLEventListener, ActionListener, Mou
         float aspect = myCanvas.getWidth() / myCanvas.getHeight();
         Matrix3D pMat = perspective(50.0f, aspect, 0.1f, 1000.0f);
 
-        // Matrix3D vMat = new Matrix3D();
 
-        //vMat.translate(-cameraX, -cameraY, -cameraZ);
-        //vMat.translate(0, 0, -size);
-        //double x = (double) ((System.currentTimeMillis() / 20) % 72000) / 200.0;
-
-
-        // push view matrix onto the stack
         mvStack.pushMatrix();
         mvStack.translate(-cameraX, -cameraY, -cameraZ);
         mvStack.translate(0, 0, -zoom);
         double amt = (double) (System.currentTimeMillis() % 360000) / 1000.0;
 
 
-        // ----------------------  pyramid == sun
+        // ----------------------   == sun
         mvStack.pushMatrix();
         mvStack.translate(skyBoxLocX, skyBoxLocY, skyBoxLocZ);
-        mvStack.scale(10, 10, 10);
-        mvStack.rotate((System.currentTimeMillis() % 3600) / 10.0, 1.0, 0.0, 0.0);
+        mvStack.scale(2, 2, 2);
+        mvStack.rotate((System.currentTimeMillis() % 3600) / 10.0,
+                (System.currentTimeMillis() % 3600) / 10.0,
+                (System.currentTimeMillis() % 3600) / 10.0, 0.0);
         gl.glUniformMatrix4fv(mv_loc, 1, false, mvStack.peek().getFloatValues(), 0);
         gl.glUniformMatrix4fv(proj_loc, 1, false, pMat.getFloatValues(), 0);
         gl.glBindBuffer(GL.GL_ARRAY_BUFFER, vbo[0]);
@@ -207,7 +139,7 @@ public class Ass2 extends JFrame implements GLEventListener, ActionListener, Mou
         gl.glFrontFace(GL_CCW);
         gl.glEnable(GL_DEPTH_TEST);
         //gl.glVertexAttrib4fv(1, noise);
-        gl.glDrawArrays(GL_TRIANGLES, 0, 36);
+        gl.glDrawArrays(GL_TRIANGLES, 0, mySphere.getIndices().length);
         mvStack.popMatrix();
 
         //-----------------------  cube == planet
@@ -224,12 +156,12 @@ public class Ass2 extends JFrame implements GLEventListener, ActionListener, Mou
         gl.glFrontFace(GL_CW);
         gl.glEnable(GL_DEPTH_TEST);
         //gl.glVertexAttrib4fv(1, noise);
-        gl.glDrawArrays(GL_TRIANGLES, 0, 36);
+        gl.glDrawArrays(GL_TRIANGLES, 0, mySphere.getIndices().length);
         mvStack.popMatrix();
 
         //-----------------------  smaller cube == moon
         mvStack.pushMatrix();
-        mvStack.translate(0.0f, Math.sin(amt) / 2.0f, Math.cos(amt) / 2.0f);
+        mvStack.translate(0.0f, Math.sin(amt) * 2, Math.cos(amt) * 2);
         mvStack.rotate((System.currentTimeMillis() % 3600) / 10.0, 0.0, 0.0, 1.0);
         mvStack.scale(0.25, 0.25, 0.25);
         gl.glUniformMatrix4fv(mv_loc, 1, false, mvStack.peek().getFloatValues(), 0);
@@ -241,101 +173,49 @@ public class Ass2 extends JFrame implements GLEventListener, ActionListener, Mou
         gl.glFrontFace(GL_CW);
         gl.glEnable(GL_DEPTH_TEST);
         //gl.glVertexAttrib4fv(1, noise);
-        gl.glDrawArrays(GL_TRIANGLES, 0, 36);
+        gl.glDrawArrays(GL_TRIANGLES, 0, mySphere.getIndices().length);
         mvStack.popMatrix();
         mvStack.popMatrix();
         mvStack.popMatrix();
-//
-//        // ------------------- draw the cube using buffer #0
-//        Matrix3D mMat = new Matrix3D();
-//        //double amt = (double) (System.currentTimeMillis()%3600)/10.0;
-//        //mMat.rotate(amt, amt, amt);
-//        mMat.translate(cubeLocX, cubeLocY, cubeLocZ);
-//        mMat.rotate(0, ++rotationi, rotationi);
-//        // mMat.rotate(0,size,0);
-//        // mMat.rotate(45*x, 0, x);
-//        //mMat.rotate(21*x, x, 0);
-//        //mMat.scale(2,2,2);
-//        //mMat.translate(Math.sin(2.1*x)*2.0,Math.cos(1.7*x)*2.0,Math.sin(1.3*x)*Math.cos(1.5*x)*2.0);
-//        Matrix3D mvMat = new Matrix3D();
-//        mvMat.concatenate(vMat);
-//        mvMat.concatenate(mMat);
-//        gl.glUniformMatrix4fv(mv_loc, 1, false, mvMat.getFloatValues(), 0);
-//        gl.glUniformMatrix4fv(proj_loc, 1, false, pMat.getFloatValues(), 0);
-//        gl.glBindBuffer(GL.GL_ARRAY_BUFFER, vbo[0]);
-//        gl.glVertexAttribPointer(0, 3, GL.GL_FLOAT, false, 0, 0);
-//        gl.glEnableVertexAttribArray(0);
-//        gl.glEnable(GL_CULL_FACE);
-//        gl.glFrontFace(GL_CW);
-//        gl.glEnable(GL_DEPTH_TEST);
-//        gl.glDepthFunc(GL_LEQUAL);
-//        gl.glVertexAttrib4fv(1, noise);
-//        gl.glDrawArraysInstanced(GL_TRIANGLES, 0, 36, 500);
-//
-//        // ------------------- draw the Skybox using buffer #1
-//        mMat = new Matrix3D();
-//        mMat.translate(skyBoxLocX, skyBoxLocY, skyBoxLocZ);
-//        mMat.rotate(rotationi, rotationi, rotationi);
-//        mMat.scale(1000, 1000, 1000);
-//        mvMat = new Matrix3D();
-//        mvMat.concatenate(vMat);
-//        mvMat.concatenate(mMat);
-//        gl.glUniformMatrix4fv(mv_loc, 1, false, mvMat.getFloatValues(), 0);
-//        gl.glUniformMatrix4fv(proj_loc, 1, false, pMat.getFloatValues(), 0);
-//        gl.glBindBuffer(GL.GL_ARRAY_BUFFER, vbo[1]);
-//        gl.glVertexAttribPointer(0, 3, GL.GL_FLOAT, false, 0, 0);
-//        gl.glEnableVertexAttribArray(0);
-//        gl.glEnable(GL_CULL_FACE);
-//        gl.glFrontFace(GL_CCW);
-//        gl.glEnable(GL_DEPTH_TEST);
-//        gl.glDepthFunc(GL_LEQUAL);
-//        gl.glVertexAttrib4fv(1, noise);
-//        gl.glDrawArraysInstanced(GL_TRIANGLES, 0, 36, 1);
+
     }
 
 
     private void setupVertices(GL4 gl) {
-        float[] cube_positions =
-                {-0.25f, 0.25f, -0.25f, -0.25f, -0.25f, -0.25f, 0.25f, -0.25f, -0.25f,
-                        0.25f, -0.25f, -0.25f, 0.25f, 0.25f, -0.25f, -0.25f, 0.25f, -0.25f,
-                        0.25f, -0.25f, -0.25f, 0.25f, -0.25f, 0.25f, 0.25f, 0.25f, -0.25f,
-                        0.25f, -0.25f, 0.25f, 0.25f, 0.25f, 0.25f, 0.25f, 0.25f, -0.25f,
-                        0.25f, -0.25f, 0.25f, -0.25f, -0.25f, 0.25f, 0.25f, 0.25f, 0.25f,
-                        -0.25f, -0.25f, 0.25f, -0.25f, 0.25f, 0.25f, 0.25f, 0.25f, 0.25f,
-                        -0.25f, -0.25f, 0.25f, -0.25f, -0.25f, -0.25f, -0.25f, 0.25f, 0.25f,
-                        -0.25f, -0.25f, -0.25f, -0.25f, 0.25f, -0.25f, -0.25f, 0.25f, 0.25f,
-                        -0.25f, -0.25f, 0.25f, 0.25f, -0.25f, 0.25f, 0.25f, -0.25f, -0.25f,
-                        0.25f, -0.25f, -0.25f, -0.25f, -0.25f, -0.25f, -0.25f, -0.25f, 0.25f,
-                        -0.25f, 0.25f, -0.25f, 0.25f, 0.25f, -0.25f, 0.25f, 0.25f, 0.25f,
-                        0.25f, 0.25f, 0.25f, -0.25f, 0.25f, 0.25f, -0.25f, 0.25f, -0.25f
-                };
-        float[] skybox_positions =
-                {-0.25f, 0.25f, -0.25f, -0.25f, -0.25f, -0.25f, 0.25f, -0.25f, -0.25f,
-                        0.25f, -0.25f, -0.25f, 0.25f, 0.25f, -0.25f, -0.25f, 0.25f, -0.25f,
-                        0.25f, -0.25f, -0.25f, 0.25f, -0.25f, 0.25f, 0.25f, 0.25f, -0.25f,
-                        0.25f, -0.25f, 0.25f, 0.25f, 0.25f, 0.25f, 0.25f, 0.25f, -0.25f,
-                        0.25f, -0.25f, 0.25f, -0.25f, -0.25f, 0.25f, 0.25f, 0.25f, 0.25f,
-                        -0.25f, -0.25f, 0.25f, -0.25f, 0.25f, 0.25f, 0.25f, 0.25f, 0.25f,
-                        -0.25f, -0.25f, 0.25f, -0.25f, -0.25f, -0.25f, -0.25f, 0.25f, 0.25f,
-                        -0.25f, -0.25f, -0.25f, -0.25f, 0.25f, -0.25f, -0.25f, 0.25f, 0.25f,
-                        -0.25f, -0.25f, 0.25f, 0.25f, -0.25f, 0.25f, 0.25f, -0.25f, -0.25f,
-                        0.25f, -0.25f, -0.25f, -0.25f, -0.25f, -0.25f, -0.25f, -0.25f, 0.25f,
-                        -0.25f, 0.25f, -0.25f, 0.25f, 0.25f, -0.25f, 0.25f, 0.25f, 0.25f,
-                        0.25f, 0.25f, 0.25f, -0.25f, 0.25f, 0.25f, -0.25f, 0.25f, -0.25f
-                };
 
+        Vertex3D[] vertices = mySphere.getVertices();
+        int[] indices = mySphere.getIndices();
+
+        float[] fvalues = new float[indices.length * 3];
+        float[] tvalues = new float[indices.length * 2];
+        float[] nvalues = new float[indices.length * 3];
+
+        for (int i = 0; i < indices.length; i++) {
+            fvalues[i * 3] = (float) (vertices[indices[i]]).getX();
+            fvalues[i * 3 + 1] = (float) (vertices[indices[i]]).getY();
+            fvalues[i * 3 + 2] = (float) (vertices[indices[i]]).getZ();
+            tvalues[i * 2] = (float) (vertices[indices[i]]).getS();
+            tvalues[i * 2 + 1] = (float) (vertices[indices[i]]).getT();
+            nvalues[i * 3] = (float) (vertices[indices[i]]).getNormalX();
+            nvalues[i * 3 + 1] = (float) (vertices[indices[i]]).getNormalY();
+            nvalues[i * 3 + 2] = (float) (vertices[indices[i]]).getNormalZ();
+        }
 
         gl.glGenVertexArrays(vao.length, vao, 0);
         gl.glBindVertexArray(vao[0]);
         gl.glGenBuffers(vbo.length, vbo, 0);
 
         gl.glBindBuffer(GL.GL_ARRAY_BUFFER, vbo[0]);
-        FloatBuffer cubeBuffer = FloatBuffer.wrap(cube_positions);
-        gl.glBufferData(GL.GL_ARRAY_BUFFER, cubeBuffer.limit() * 4, cubeBuffer, GL.GL_STATIC_DRAW);
+        FloatBuffer vertBuf = FloatBuffer.wrap(fvalues);
+        gl.glBufferData(GL.GL_ARRAY_BUFFER, vertBuf.limit() * 4, vertBuf, GL.GL_STATIC_DRAW);
 
         gl.glBindBuffer(GL.GL_ARRAY_BUFFER, vbo[1]);
-        FloatBuffer skyboxBuffer = FloatBuffer.wrap(skybox_positions);
-        gl.glBufferData(GL.GL_ARRAY_BUFFER, skyboxBuffer.limit() * 4, skyboxBuffer, GL.GL_STATIC_DRAW);
+        FloatBuffer texBuf = FloatBuffer.wrap(tvalues);
+        gl.glBufferData(GL.GL_ARRAY_BUFFER, texBuf.limit() * 4, texBuf, GL.GL_STATIC_DRAW);
+
+        gl.glBindBuffer(GL.GL_ARRAY_BUFFER, vbo[2]);
+        FloatBuffer norBuf = FloatBuffer.wrap(nvalues);
+        gl.glBufferData(GL.GL_ARRAY_BUFFER, norBuf.limit() * 4, norBuf, GL.GL_STATIC_DRAW);
     }
 
 
@@ -431,10 +311,10 @@ public class Ass2 extends JFrame implements GLEventListener, ActionListener, Mou
 //            if (g != 2) g = 2;
 //            else g = 1;
 //        } else
-        if ("noise".equals(e.getActionCommand())) {
-            if (n != 0.0) n = 0.0f;
-            else n = 1.0f;
-        }
+//        if ("noise".equals(e.getActionCommand())) {
+//            if (n != 0.0) n = 0.0f;
+//            else n = 1.0f;
+//        }
 //        else if ("autozoom".equals(e.getActionCommand())) {
 //            autozoom = !autozoom;
 //        }
@@ -470,16 +350,16 @@ public class Ass2 extends JFrame implements GLEventListener, ActionListener, Mou
     private class zoomIn extends AbstractAction {
         @Override
         public void actionPerformed(ActionEvent e) {
-            Ass2.zoom += 1.0f;
-            System.out.println("zoom + 1.0");
+            Ass2.zoom -= 1.0f;
+            //System.out.println("zoom + 1.0");
         }
     }
 
     private class zoomOut extends AbstractAction {
         @Override
         public void actionPerformed(ActionEvent e) {
-            Ass2.zoom -= 1.0f;
-            System.out.println("zoom - 1.0");
+            Ass2.zoom += 1.0f;
+            //System.out.println("zoom - 1.0");
         }
     }
 }
